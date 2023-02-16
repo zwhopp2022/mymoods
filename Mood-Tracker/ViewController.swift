59e9dc4a59ef751559c44dfdacb8a7ae631c99d2
Zachary Hopp <zacharyhopp01@gmail.com>
2023-02-16 14:35:19 -0500

    testing removing templates

---
+++ Mood-Tracker/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Mood-Tracker
+//
+//  Created by Zachary Hopp on 2/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
---
+++ MoodTrackerTests/MoodTrackerTests.swift
@@ -0,0 +1,13 @@
+//
+//  MoodTrackerTests.swift
+//  MoodTrackerTests
+//
+//  Created by Zachary Hopp on 2/16/23.
+//
+import XCTest
+@testable import Mood_Tracker
+
+class MoodTrackerTests: XCTestCase {
+
+    // MARK: Class Tests
+}
