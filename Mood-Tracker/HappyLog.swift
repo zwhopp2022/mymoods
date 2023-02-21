627deeeab66cbf7a20d5cc41b0d3f9099288a9da
Jack McLean <jackmclean@n3-38-2.dhcp.drexel.edu>
2023-02-20 23:07:38 -0500

    Revised feelings input pages, all six faces

---
+++ Mood-Tracker/HappyLog.swift
@@ -0,0 +1,56 @@
+//
+//  HappyLog.swift
+//  Mood-Tracker
+//
+//  Created by Jack McLean on 2/20/23.
+//
+
+import SwiftUI
+
+struct HappyLog: View {
+    @State private var userInput = ""
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.orange
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.8)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.4)
+                    .foregroundColor(.white.opacity(0.15))
+                VStack {
+                    Image("Happy")
+                        .frame(height: 125)
+                    Text("What's going on?")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Yay! Want to write it down?", text: $userInput)
+                        .padding()
+                        .frame(width: 300, height: 100)
+                        .multilineTextAlignment(.center)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                    Button("Store") {
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.black.opacity(0.5))
+                    
+                    Spacer()
+                        .frame(height: 125)
+                }
+            }
+        }
+        .navigationBarHidden(true)
+    }
+}
+
+struct HappyLog_Previews: PreviewProvider {
+    static var previews: some View {
+        HappyLog()
+    }
+}
