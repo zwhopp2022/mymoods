118c07039b7d7f59fd946b68a8d9a903788a7edb
Zachary Hopp <zacharyhopp01@gmail.com>
2023-02-20 23:40:20 -0500

    Linked FacesView.swift with Log pages

--- Mood-Tracker/FacesView.swift
+++ Mood-Tracker/FacesView.swift
@@ -10,52 +10,69 @@ import SwiftUI
 // background color gradient
 let backgroundGradient = LinearGradient(colors: [Color.blue, Color.green, Color.yellow, Color.red], startPoint: .top, endPoint: .bottom)
 
+
 struct FacesView: View {
+    @State private var moveToSaddest = false
+    @State private var moveToHappiest = false
     var body: some View {
-        ZStack {
-            backgroundGradient.ignoresSafeArea()
-                .blur(radius: 100)
-            VStack{
-                Text("How are you doing?")
-                    .bold()
-                    .foregroundColor(.black)
-                    .font(.system(size: 38))
-                Spacer()
-                HStack(spacing: -80){
-                    // happiest button
-                    Button(action:{
-                        // no action yet
-                    }){ Image("Happiest") }
-                    // Saddest button
-                    Button(action: {
-                        // no action yet
-                    }) {Image("Saddest")}
-                }
-                HStack(spacing: -80){
-                    // Happy button
-                    Button(action:{
-                        // no action yet
-                    }){ Image("Happy") }
-                    // sad button
-                    Button(action: {
-                        // no action yet
-                    }) {Image("Sad")}
-                }
-                HStack(spacing: -80){
-                    // Neutral button
-                    Button(action:{
-                        // no action yet
-                    }){ Image("neutral") }
-                    // Angriest button
-                    Button(action: {
-                        // no action yet
-                    }) {Image("Angriest")}
-                }
-                Spacer()
-                    .frame(height:140)
-            }.padding()
+        NavigationStack{
+            ZStack {
+                backgroundGradient.ignoresSafeArea()
+                    .blur(radius: 100)
+                VStack{
+                    Text("How are you doing?")
+                        .bold()
+                        .foregroundColor(.black)
+                        .font(.system(size: 38))
+                    Spacer()
+                    HStack(spacing: -80){
+                        // happiest button
+                        NavigationLink {
+                            HappiestLog()
+                        } label: {
+                            Image("Happiest")
+                        }
+                        // saddest button
+                        NavigationLink {
+                            SaddestLog()
+                        } label: {
+                            Image("Saddest")
+                        }
+                    }
+                    HStack(spacing: -80){
+                        // Happy button
+                        NavigationLink {
+                            HappyLog()
+                        } label: {
+                            Image("Happy")
+                        }
+                        // sad button
+                        NavigationLink {
+                            SadLog()
+                        } label: {
+                            Image("Sad")
+                        }
+                    }
+                    HStack(spacing: -80){
+                        // Neutral button
+                        NavigationLink {
+                            NeutralLog()
+                        } label: {
+                            Image("neutral")
+                        }
+                        // Angriest button
+                        NavigationLink {
+                            AngriestLog()
+                        } label: {
+                            Image("Angriest")
+                        }
+                    }
+                    Spacer()
+                        .frame(height:140)
+                }.padding()
             }
         }
+    }
     }
 
 struct FacesView_Previews: PreviewProvider {
