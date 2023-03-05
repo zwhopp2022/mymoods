0834a002db8f357e5e72c734897d093844b54781
Zachary Hopp <zacharyhopp01@gmail.com>
2023-03-05 15:05:10 -0500

    recreated database in sqlitemodule

---
+++ Database/MoodDB.swift
@@ -0,0 +1,96 @@
+//
+//  MoodDB.swift
+//  Mood-Tracker
+//
+//  Created by Zachary Hopp on 3/5/23.
+//
+
+import SQLite
+// create database in documents directory
+let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+
+let moodDB = try! Connection("\(path)/moodDB.sqlite3")
+
+class MoodDatabase {
+    
+    // initialize variables needed for an instance of MoodDatabase
+    var moodDB: Connection!
+    var happiest: Table!
+    var saddest: Table!
+    var sad: Table!
+    var neutral: Table!
+    var angriest: Table!
+    var happy: Table!
+    var everything: Table!
+    var mood: Expression<String>!
+    var reason: Expression<String>!
+    
+    
+    init () {
+        do {
+            // create database in user's documents directory
+            let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            moodDB = try! Connection("\(path)/moodDB.sqlite3")
+            
+            // create column names and their associated attribute type
+            mood = Expression<String>("mood")
+            reason = Expression<String>("reason")
+            
+            
+            // create 6 mood tables and one everything table
+            happiest = Table("happiest")
+            saddest = Table("saddest")
+            sad = Table("sad")
+            neutral = Table("neutral")
+            angriest = Table("angriest")
+            happy = Table("happy")
+            everything = Table("everything")
+            if (!UserDefaults.standard.bool(forKey: "databaseCreated")) {
+                try moodDB.run(happiest.create { t in
+                    t.column(mood)
+                    t.column(reason)
+                })
+                
+                try moodDB.run(saddest.create { t in
+                    t.column(mood)
+                    t.column(reason)
+                })
+                
+                try moodDB.run(sad.create { t in
+                    t.column(mood)
+                    t.column(reason)
+                })
+                
+                try moodDB.run(neutral.create { t in
+                    t.column(mood)
+                    t.column(reason)
+                })
+                
+                try moodDB.run(angriest.create { t in
+                    t.column(mood)
+                    t.column(reason)
+                })
+                
+                try moodDB.run(happy.create { t in
+                    t.column(mood)
+                    t.column(reason)
+                })
+                
+                try moodDB.run(everything.create { t in
+                    t.column(mood)
+                    t.column(reason)
+                })
+                UserDefaults.standard.set(true, forKey: "databaseCreated")
+            }
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+}
+
+
+
+
+
