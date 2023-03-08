c9521c5c90a0386d2b7b289e3978ad00046f86f7
Jack McLean <jackmclean@n3-38-2.dhcp.drexel.edu>
2023-03-08 00:29:14 -0500

    Updated branch for my task each display page for input
    (cherry picked from commit 3835533477a8726ffa7c3a1b15c5a4f6736e8c60)

---
+++ Mood-Tracker/HappiestDisplay.swift
@@ -0,0 +1,55 @@
+//  HappiestDisplay.swift
+//  Mood-Tracker
+//
+//  Created by Rudra Pratap Singh on 3/3/23.
+//
+
+import SwiftUI
+
+struct HappiestDisplay: View {
+    
+    @State private var isScrollExpanded = false
+    @State private var selectedScrollNum = 1
+    
+    var body: some View {
+        
+        VStack(alignment: .leading, spacing: 20){
+            Text("When you were the Happiest")
+                .font(.system(size:28))
+            Text("Your Activities")
+                .font(.title3)
+            DisclosureGroup("\(selectedScrollNum)", isExpanded:
+                $isScrollExpanded) {
+                ScrollView {
+                VStack {
+                    ForEach(1...500, id: \.self) { num in
+                        Text("\(num)")
+                            .frame(maxWidth: .infinity)
+                            .font(.title3)
+                            .padding(.all).onTapGesture {
+                                self.selectedScrollNum = num
+                                withAnimation {
+                                    self.isScrollExpanded.toggle()
+                
+                                }
+                            }
+                    }
+                }
+            }.frame(height:200)
+            }.accentColor(.white)
+             .font(.title2)
+             .foregroundColor(.white)
+             .padding(.all)
+             .background(Color.blue)
+             .cornerRadius(20)
+            
+            Spacer()
+        }.padding(.all)
+    }
+}
+
+struct HappiestDisplay_Previews: PreviewProvider {
+    static var previews: some View {
+        HappiestDisplay()
+    }
+}
---
+++ Mood-Tracker/HappyDisplay.swift
@@ -0,0 +1,56 @@
+//
+//  HappyDisplay.swift
+//  Mood-Tracker
+//
+//  Created by Rudra Pratap Singh on 3/3/23.
+//
+
+import SwiftUI
+
+struct HappyDisplay: View {
+    
+    @State private var isScrollExpanded = false
+    @State private var selectedScrollNum = 1
+    
+    var body: some View {
+        
+        VStack(alignment: .leading, spacing: 20){
+            Text("When you were Happy")
+                .font(.largeTitle)
+            Text("Your Activities")
+                .font(.title3)
+            DisclosureGroup("\(selectedScrollNum)", isExpanded:
+                $isScrollExpanded) {
+                ScrollView {
+                VStack {
+                    ForEach(1...500, id: \.self) { num in
+                        Text("\(num)")
+                            .frame(maxWidth: .infinity)
+                            .font(.title3)
+                            .padding(.all).onTapGesture {
+                                self.selectedScrollNum = num
+                                withAnimation {
+                                    self.isScrollExpanded.toggle()
+                
+                                }
+                            }
+                    }
+                }
+            }.frame(height:200)
+            }.accentColor(.white)
+             .font(.title2)
+             .foregroundColor(.white)
+             .padding(.all)
+             .background(Color.blue)
+             .cornerRadius(20)
+            
+            Spacer()
+        }.padding(.all)
+    }
+}
+
+struct HappyDisplay_Previews: PreviewProvider {
+    static var previews: some View {
+        HappyDisplay()
+    }
+}
