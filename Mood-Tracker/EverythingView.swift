217a00e51861922d2a0be3da18d666e9b57cd6eb
Zachary Hopp <zacharyhopp01@gmail.com>
2023-03-08 14:22:52 -0500

    Began making Every mood page

---
+++ Mood-Tracker/EverythingView.swift
@@ -0,0 +1,20 @@
+//
+//  EverythingView.swift
+//  Mood-Tracker
+//
+//  Created by Zachary Hopp on 3/7/23.
+//
+
+import SwiftUI
+
+struct EverythingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EverythingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EverythingView()
+    }
+}
