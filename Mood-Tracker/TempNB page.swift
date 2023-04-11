3f1162a7749d9992fcf182f9482ec30c15397f67
Rudra Pratap Singh <rps68@drexl.edu>
2023-04-10 23:28:03 -0400

    Created the Nav bar and linked the icons to their respective pages

---
+++ Mood-Tracker/TempNB page.swift
@@ -0,0 +1,20 @@
+//
+//  TempNB page.swift
+//  Mood-Tracker
+//
+//  Created by Rudra Pratap Singh on 4/10/23.
+//
+
+import SwiftUI
+
+struct TempNB_page: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TempNB_page_Previews: PreviewProvider {
+    static var previews: some View {
+        TempNB_page()
+    }
+}
