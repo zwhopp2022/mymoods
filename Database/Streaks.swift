b3f103078d9cba88c3c9271b93cff52b37b0f379
Jack McLean <jackmclean@n3-77-132.dhcp.drexel.edu>
2023-04-19 11:09:43 -0400

    Created files for streaks

---
+++ Database/Streaks.swift
@@ -0,0 +1,9 @@
+//
+//  Streaks.swift
+//  Mood-Tracker
+//
+//  Created by Jack McLean on 4/19/23.
+//
+
+import Foundation
+
---
+++ Mood-Tracker/StreaksView.swift
@@ -0,0 +1,20 @@
+//
+//  StreaksView.swift
+//  Mood-Tracker
+//
+//  Created by Jack McLean on 4/19/23.
+//
+
+import SwiftUI
+
+struct StreaksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StreaksView_Previews: PreviewProvider {
+    static var previews: some View {
+        StreaksView()
+    }
+}
