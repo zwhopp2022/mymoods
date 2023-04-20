b7c52a24d9a0b5accd87cc3abba842d1b5afcf0e
Jack McLean <jackmclean@n3-77-132.dhcp.drexel.edu>
2023-04-20 19:03:39 -0400

    Added pie charts and finishing up functions
    (cherry picked from commit a66d28eff4f2ad5180f8a470c352cdf4efd2fdd0)

--- Database/Streaks.swift
+++ Database/Streaks.swift
@@ -8,11 +8,16 @@
 import Foundation
 
 public func streaks() -> String{
-   // let _: String = moodDB.retrieveReasons(moodTable: "everything").last?.timeDate ?? "<no date>"
+    // let _: String = moodDB.retrieveReasons(moodTable: "everything").last?.timeDate ?? "<no date>"
     let listOfDate = moodDB.retrieveReasons(moodTable: "everything")
     var lastTwo = listOfDate.suffix(2)
-    var date = lastTwo.first
-    var secondToLastDate = date?.timeDate
-    return secondToLastDate ?? "<no date>"
+    var lastLog = lastTwo.first
+    var recentLog = lastTwo.last
+    var lastMonth = lastLog?.numMonth
+    var recentMonth = recentLog?.numMonth
+    var lastDay = lastLog?.numDay
+    var recentDay = recentLog?.numDay
+    
+    
 }
 
