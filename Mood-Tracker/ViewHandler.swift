24c13c57b9c63a91dc9ebc1ad4a1d4111d998219
Rainier <rrp75@drexel.edu>
2023-05-05 14:46:53 -0400

    navbar 2.0 in progress

--- Mood-Tracker/ViewHandler.swift
+++ Mood-Tracker/ViewHandler.swift
@@ -8,28 +8,39 @@
 import Foundation
 import SwiftUI
 
+//struct ViewHandler: View {
+//    var body: some View {
+//        TabView {
+//            FacesView()
+//                .tabItem() {
+//                    Image("smile")
+//                }
+//            StreaksView()
+//                .tabItem() {
+//                    Image("medal")
+//                }
+//            AnalyticsView()
+//                .tabItem() {
+//                    Image("pie")
+//                }
+//            EverythingView()
+//                .tabItem() {
+//                    Image("book")
+//                }
+//        }
+//    }
+//}
+
+
 struct ViewHandler: View {
     var body: some View {
-        TabView {
-            FacesView()
-                .tabItem() {
-                    Image("smile")
-                }
-            StreaksView()
-                .tabItem() {
-                    Image("medal")
-                }
-            AnalyticsView()
-                .tabItem() {
-                    Image("pie")
-                }
-            EverythingView()
-                .tabItem() {
-                    Image("book")
-                }
-        }
+        NavigationView {
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading)
+            }
     }
 }
+
 
 struct NavBarView_Previews: PreviewProvider {
     static var previews: some View {
